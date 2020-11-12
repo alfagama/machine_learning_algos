930ca81196a418fe6e285d17b470c090198d7d49
arampatzisgiorgos <48099515+arampatzisgiorgos@users.noreply.github.com>
2020-11-12 21:23:09 +0200

    v4.0

---
+++ 4 - KNN/KNN_Template.py
@@ -0,0 +1,69 @@
+# =============================================================================
+# HOMEWORK 4 - INSTANCE-BASED LEARNING
+# K-NEAREST NEIGHBORS TEMPLATE
+# Complete the missing code by implementing the necessary commands.
+# For ANY questions/problems/help, email us: arislaza@csd.auth.gr, ipierros@csd.auth.gr
+# =============================================================================
+
+# import the KNeighborsClassifier
+# if you want to do the hard task, also import the KNNImputer
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+random.seed = 42
+np.random.seed(666)
+
+
+
+# Import the titanic dataset
+# Decide which features you want to use (some of them are useless, ie PassengerId).
+#
+# Feature 'Sex': because this is categorical instead of numerical, KNN can't deal with it, so drop it
+# Note: another solution is to use one-hot-encoding, but it's out of the scope for this exercise.
+#
+# Feature 'Age': because this column contains missing values, KNN can't deal with it, so drop it
+# If you want to do the harder task, don't drop it.
+#
+# =============================================================================
+titanic =
+
+
+
+# Normalize feature values using MinMaxScaler
+# Fit the scaler using only the train data
+# Transform both train and test data.
+# =============================================================================
+scaler =
+
+
+
+# Do the following only if you want to do the hard task.
+#
+# Perform imputation for completing the missing data for the feature
+# 'Age' using KNNImputer. 
+# As always, fit on train, transform on train and test.
+#
+# Note: KNNImputer also has a n_neighbors parameter. Use n_neighbors=3.
+# =============================================================================
+imputer =
+
+
+
+# Create your KNeighborsClassifier models for different combinations of parameters
+# Train the model and predict. Save the performance metrics.
+# =============================================================================
+
+
+
+# Plot the F1 performance results for any combination οf parameter values of your choice.
+# If you want to do the hard task, also plot the F1 results with/without imputation (in the same figure)
+# =============================================================================
+plt.title('k-Nearest Neighbors (Weights = '<?>', Metric = '<?>', p = <?>)')
+plt.plot(f1_impute, label='with impute')
+plt.plot(f1_no_impute, label='without impute')
+plt.legend()
+plt.xlabel('Number of neighbors')
+plt.ylabel('F1')
+plt.show()
+
