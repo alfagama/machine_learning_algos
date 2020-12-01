b3ced1c6cbe566d60e6453a50b488508f9e0ce99
arampatzisgiorgos <48099515+arampatzisgiorgos@users.noreply.github.com>
2020-12-01 21:04:32 +0200

    v6.3

--- 6 - Model Evaluation/Friedman.py
+++ 6 - Model Evaluation/Friedman.py
@@ -9,20 +9,14 @@ svm = data_set['Kernel']
 cn2 = data_set['CN2']
 
 stat, p = fr(c45, oNN, mNB, svm, cn2)
-# print(stat, p)
 print('Statistics=%.3f, p=%.11f' % (stat, p))
 
 alpha = [0.0001, 0.001, 0.01, 0.1, 0.5, 1]
 
 for a in alpha:
-    if p > a:
-        print('Same distributions (fail to reject H0), for alpha = ', a)
-    else:
-        print('Different distributions (reject H0), for alpha = ', a)
-
-# print(data_set.head(30))
-# print(c45.head(30))
-# print(oNN.head(30))
-# print(mNB.head(30))
-# print(svm.head(30))
-# print(cn2.head(30))
+    if p >= a:
+        # print('Same distributions (fail to reject H0), for alpha = ', a)
+        print('Algorithms do not have statistical difference, for alpha = ', a)
+    elif p < a:
+        # print('Different distributions (reject H0), for alpha = ', a)
+        print('Algorithms have statistical difference, for alpha = ', a)
