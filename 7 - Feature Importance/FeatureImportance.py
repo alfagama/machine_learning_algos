8e17abe8be9fd7fb85ae151695804f00747dc8b4
arampatzisgiorgos <48099515+arampatzisgiorgos@users.noreply.github.com>
2020-12-06 01:46:37 +0200

    v7.1

---
+++ 7 - Feature Importance/FeatureImportance.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+htru2 = pd.read_csv("HTRU_2.csv",
+                      sep=',',
+                      header=None,  # no header, alternative header = header_col
+                      # index_col=None,  # no index, alternative header = index_row
+                      # skiprows=0  # how many rows to skip / not include in read_csv
+                      )
+pd.set_option('display.max_columns', None)
+
+print(htru2.head(5))
+
+X = htru2.iloc[:, -len(htru2):-1]
+y = htru2.iloc[:, -1]
+
+# a = 0
+# b = 0
+# for i in y:
+#     if i == 1:
+#         a += 1
+#     elif i == 0:
+#         b += 1
+#     else:
+#         print(i)
+# print(a) # 1639
+# print(b) # 16259
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, random_state=42, train_size=0.80)
+
+model = RandomForestClassifier(
+    n_estimators=100,
+    criterion='gini',
+    max_depth=3,
+    random_state=1
+)
+
+model.fit(X_train, y_train)
+
+y_predicted = model.predict(X_test)
+
+print("Random Forest - Model Evaluation: ")
+print("Accuracy: ", metrics.accuracy_score(y_test, y_predicted))
+print("Precision:", metrics.precision_score(y_test, y_predicted, average="macro"))
+print("Recall: ", metrics.recall_score(y_test, y_predicted, average="macro"))
+print("F1: ", metrics.f1_score(y_test, y_predicted, average="macro"))
