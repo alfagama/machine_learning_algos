cd3acaf5a17d70d1343c71a0d4c2cd56c4cdf15e
arampatzisgiorgos <48099515+arampatzisgiorgos@users.noreply.github.com>
2020-12-12 00:16:37 +0200

    v7.4

--- 7 - Feature Importance/FeatureImportance.py
+++ 7 - Feature Importance/FeatureImportance.py
@@ -62,6 +62,24 @@ print("AUC: ", metrics.auc(false_positive_rate, true_positive_rate))
 print("ROC AUC: ", metrics.roc_auc_score(y_test, y_predicted))
 
 # =============================================================================
+# PCA
+# =============================================================================
+
+
+# =============================================================================
+# print metrics (ACC, PRE, REC, F1, AUC, ROC AUC) after PCA
+# =============================================================================
+print("Random Forest - Model Evaluation - After PCA: ")
+print("Accuracy: ", metrics.accuracy_score(y_test, y_predicted))
+print("Precision:", metrics.precision_score(y_test, y_predicted, average="macro"))
+print("Recall: ", metrics.recall_score(y_test, y_predicted, average="macro"))
+print("F1: ", metrics.f1_score(y_test, y_predicted, average="macro"))
+false_positive_rate, true_positive_rate, thresholds = metrics.roc_curve(y_test, y_predicted)
+print("AUC: ", metrics.auc(false_positive_rate, true_positive_rate))
+# may differ sometimes, link: https://stackoverflow.com/questions/31159157/different-result-with-roc-auc-score-and-auc
+print("ROC AUC: ", metrics.roc_auc_score(y_test, y_predicted))
+
+# =============================================================================
 # feature importance : method 1: Random Forest Built-in Feature Importance
 # =============================================================================
 print()
